d79e9df9d53c241efe9413cd0aa2a80115f211c4
Deckles <declan.caveney@gmail.com>
2017-02-10 22:11:58 +0000

    Added comment

--- bd_kill.bat
+++ bd_kill.bat
@@ -6,6 +6,8 @@ echo Launching Black Desert Online
 :: Change this to point to your BDO launcher executable
 start /b "" "D:\Games\BDO\Black Desert Online Launcher.exe"
 
+:: Make sure you're connected to a server before this number of seconds passes. 
+:: This number can be changed to start the script at a different time.
 timeout /t 180
 
 :start
