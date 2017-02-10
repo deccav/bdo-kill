e61814a140172d8f0c7b0d1dc4f66be36a514ba0
Deckles <declan.caveney@gmail.com>
2017-02-10 00:30:52 +0000

    Initial IP finder merge.

--- bd_kill_2.bat
+++ bd_kill_2.bat
@@ -4,9 +4,16 @@ cls
 
 echo Script Started
 
+for /f "tokens=2" %%a in ('
+    tasklist /FI "IMAGENAME eq BlackDesert64.exe" ^| findstr /i "BlackDesert64.exe"
+') do set ProcessId=%%a
+for /f "tokens=3,5" %%a in ('
+    netstat -on
+') do if %%b==%ProcessId% echo %%a
+
 netstat > C:\bdnetstat.txt
 
-for /f "tokens=3" %%f in ('find /c /i "5.79.111.125" "C:\bdnetstat.txt"') do set bdProcess=%%f
+for /f "tokens=3" %%f in ('find /c /i "%%a" "C:\bdnetstat.txt"') do set bdProcess=%%f
 
 echo %bdProcess%
 
